a185c327d3c82071fa7b09d40917b7414b5ea0ce
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-20 14:50:12 +0300

    Update mkdist.cmd

--- mkdist.cmd
+++ mkdist.cmd
@@ -1,28 +1,22 @@
-@echo off
+if exist c:\rtest rd /s /q c:\rtest
+md c:\rtest
+md c:\rtest\bin
+md c:\rtest\source2
+xcopy /s ..\..\..\game\source2 c:\rtest\source2
+xcopy ..\..\..\game\bin\rendersystemtest.exe c:\rtest
 
-set "game_dir=..\..\..\game"
-set "target_dir=c:\rtest"
+copy runbenchmarks.cmd c:\rtest
+copy macrun.sh c:\rtest
+copy readme.txt c:\rtest
 
-if exist "%target_dir%" rd /s /q "%target_dir%"
-md "%target_dir%"
-md "%target_dir%\bin"
-md "%target_dir%\source2"
+for %%f in ( inputsystem worldrenderer filesystem_stdio ) do copy ..\..\..\game\bin\%%f.dll c:\rtest\bin
 
-xcopy /s "%game_dir%\source2" "%target_dir%\source2"
-copy "%game_dir%\bin\rendersystemtest.exe" "%target_dir%"
+xcopy ..\..\..\game\bin\rendersystem*.dll c:\rtest\bin
+xcopy ..\..\..\game\bin\*.dylib c:\rtest\bin
 
-copy runbenchmarks.cmd "%target_dir%"
-copy macrun.sh "%target_dir%"
-copy readme.txt "%target_dir%"
+md c:\rtest\bin\rendersystemtest.app
+xcopy /s ..\..\..\game\bin\rendersystemtest.app\*.* c:\rtest\bin\rendersystemtest.app
 
-for %%f in (inputsystem worldrenderer filesystem_stdio) do copy "%game_dir%\bin\%%f.dll" "%target_dir%\bin"
+for %%f in ( tier0 vstdlib) do copy ..\..\..\game\bin\%%f.dll c:\rtest\bin
 
-xcopy "%game_dir%\bin\rendersystem*.dll" "%target_dir%\bin"
-xcopy "%game_dir%\bin\*.dylib" "%target_dir%\bin"
-
-md "%target_dir%\bin\rendersystemtest.app"
-xcopy /s "%game_dir%\bin\rendersystemtest.app\*" "%target_dir%\bin\rendersystemtest.app"
-
-for %%f in (tier0 vstdlib) do copy "%game_dir%\bin\%%f.dll" "%target_dir%\bin"
-
-..\..\pkzip25 -add -dir=relative "%target_dir%\rtest.zip" "%target_dir%\*.*"
+..\..\pkzip25 -add -dir=relative c:\rtest\rtest.zip c:\rtest\*.*
